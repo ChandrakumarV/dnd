89abd523efa0c12cc46d5e800f06bfb5182266f7
ChandrakumarV <chandrakumar252004@gmail.com>
2025-03-27 23:55:36 +0530

    fix all logic on sortable

--- src/components/sortable/index.tsx
+++ src/components/sortable/index.tsx
@@ -19,8 +19,9 @@ import { useState } from "react";
 import { SortableItem } from "./SortableItem";
 
 export function Sortable() {
-  const [items, setItems] = useState<UniqueIdentifier[]>([1, 2, 3]);
+  const [items, setItems] = useState<UniqueIdentifier[]>([1, 2, 3, 4, 5]);
   const [activeID, setActiveID] = useState<UniqueIdentifier | null>(null);
+
   const sensors = useSensors(
     useSensor(PointerSensor),
     useSensor(KeyboardSensor, {
@@ -30,15 +31,17 @@ export function Sortable() {
 
   function handleDragEnd(event: DragEndEvent) {
     const { active, over } = event;
+    if (!over || active.id === over.id) return;
 
-    if (active.id !== over?.id) {
-      setItems((items) => {
-        const oldIndex = items.indexOf(active?.id);
-        const newIndex = items.indexOf(over?.id ?? "");
+    setItems((items) => {
+      const oldIndex = items.indexOf(active.id);
+      const newIndex = items.indexOf(over.id);
+      return oldIndex !== -1 && newIndex !== -1
+        ? arrayMove(items, oldIndex, newIndex)
+        : items;
+    });
 
-        return arrayMove(items, oldIndex, newIndex);
-      });
-    }
+    setActiveID(null);
   }
 
   return (
@@ -46,23 +49,18 @@ export function Sortable() {
       sensors={sensors}
       collisionDetection={closestCenter}
       onDragEnd={handleDragEnd}
-      onDragStart={(e) => {
-        setActiveID(e.active.id);
-      }}
+      onDragStart={(e) => setActiveID(e.active.id)}
     >
       <SortableContext items={items} strategy={verticalListSortingStrategy}>
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-2 rounded-lg bg-gray-100 p-4 shadow-md">
           {items.map((id) => (
             <SortableItem key={id} id={id} />
           ))}
         </div>
       </SortableContext>
-      <DragOverlay
-        dropAnimation={{
-          duration: 1000,
-        }}
-      >
-        {activeID && <SortableItem id={activeID} />}
+
+      <DragOverlay>
+        {activeID && <SortableItem id={activeID} isOverlay />}
       </DragOverlay>
     </DndContext>
   );
